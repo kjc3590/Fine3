32d4f42a4a6cc02a33c6f645c342f6d71979b13a
kjc3590 <kjc3590@naver.com>
2017-06-30 17:05:32 +0900

    contact.jsp header.jsp 스크립트 수정

--- Fine3/WebContent/assets/js/map.js
+++ Fine3/WebContent/assets/js/map.js
@@ -1,34 +1,29 @@
 
 $(document).ready(function() {
-
-
-
-var mapCanvas = document.getElementById('map-canvas');
-var mapProp = {
-  center: new google.maps.LatLng(37.497604, 127.040720),
-  zoom:15,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-var map = new google.maps.Map(mapCanvas,mapProp);
-
-// Create a marker and set its position.
-  var marker = new google.maps.Marker({
-       
- 
-    position: new google.maps.LatLng(37.497604, 127.040720),
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapProp = {
+      center: new google.maps.LatLng(37.543917, 127.061252),
+      zoom:15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
     
-    title: '이안 베스트 !'
-  });
+    var map = new google.maps.Map(mapCanvas,mapProp);
+    
+  // Create a marker and set its position.
+      var marker = new google.maps.Marker({
+      	  
+     
+        position: new google.maps.LatLng(37.543917, 127.061252),
+        
+        title: '이안 베스트 !'
+      });
 
-marker.setMap(map);
+  marker.setMap(map);
 
 
-setTimeout(function(){
+  setTimeout(function(){
 
-  google.maps.event.trigger(map,'resize');
-},100);
-/*종찬지도끝 170227  */   
-
+      google.maps.event.trigger(map,'resize');
+  },100);
 });
 
